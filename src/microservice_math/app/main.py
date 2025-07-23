246e726e3e2db52caeed9822ec5a44e47645a8c0
andr3iyw <popoviciandrei420@gmail.com>
2025-07-23 14:25:28 +0300

    auth.

--- src/microservice_math/app/main.py
+++ src/microservice_math/app/main.py
@@ -1,16 +1,21 @@
-from flask import Flask
+from flask import Flask, session, redirect, url_for
 import asyncio
 
-from app.db import init_db
-from app.views.math_routes import math_blueprint
+from .db import init_db
+from .views.math_routes import math_blueprint
+from .views.auth_routes import auth_blueprint
 
 def create_app():
     app = Flask(__name__)
+    app.secret_key = "supersecretkey"  # Change this in production
     app.register_blueprint(math_blueprint, url_prefix="/math")
+    app.register_blueprint(auth_blueprint, url_prefix="/auth")
 
     @app.route("/")
     def root():
-        return {"message": "Math microservice running..."}
+        if not ("username" in session):
+            return redirect(url_for("auth.login"))
+        return {"message": f"Math microservice running for {session['username']}..."}
 
     return app
 
